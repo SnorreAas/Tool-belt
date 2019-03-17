f36822f447fe607713851d9c648a3250665dc291
Snorre Aas <fotballsnorre@hotmail.com>
2019-03-17 15:00:42 +0100

    Fixed some issues, added hover social icons

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import DividerFoot from '../../images/DividerFoot.png';
-import Divider from '../../images/Divider.png';
 import Github from '../../images/Github.png';
 import Facebook from '../../images/Facebook.png';
 import Linkedin from '../../images/Linkedin.png';
@@ -21,9 +20,9 @@ class Footer extends Component {
                 </div>
                 <div className="row">
                     <div className="col" style={{maxHeight: '60px', paddingTop: '5px'}}>
-                        <a target="_blank" href="https://github.com/SnorreAas"><img className="socialIcons" src={Github} alt={"Github"} /></a>
-                        <a target="_blank" href="https://www.facebook.com/snorre.aas.5"><img className="socialIcons" src={Facebook} alt={"Facebook"} /></a>
-                        <a target="_blank" href="https://www.linkedin.com/in/snorre-aas-432b4a6b"><img className="socialIcons" src={Linkedin} alt={"Linkedin"} /></a>
+                        <a href="https://github.com/SnorreAas"><img className="socialIcons" src={Github} alt={"Github"} /></a>
+                        <a href="https://www.facebook.com/snorre.aas.5"><img className="socialIcons" src={Facebook} alt={"Facebook"} /></a>
+                        <a href="https://www.linkedin.com/in/snorre-aas-432b4a6b"><img className="socialIcons" src={Linkedin} alt={"Linkedin"} /></a>
                     </div>
                 </div>
             </div>
